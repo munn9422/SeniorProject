2bd02b3d1d310bcf4858203894820864741e33c2
munn9422 <munn9422@bears.unco.edu>
2018-04-25 17:26:08 -0600

    worked on add classroom feature

--- ClassroomManagementApplication/ClassroomManagementApplication/Controllers/ClassroomController.cs
+++ ClassroomManagementApplication/ClassroomManagementApplication/Controllers/ClassroomController.cs
@@ -1,19 +1,51 @@
 ﻿using ClassroomManagementApplication.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using ClassroomManagementApplication.Models;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity;
 
 namespace ClassroomManagementApplication.Controllers
 {
     public class ClassroomController : Controller
     {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
         public ClassroomController()
         {
-
         }
+
+        public ClassroomController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
         // GET: Classroom
         public ActionResult Index()
         {
@@ -21,24 +53,29 @@ namespace ClassroomManagementApplication.Controllers
         }
 
         // GET: Classroom/Add
+        [Authorize]
         public ActionResult Add()
         {
             return View();
         }
 
         //POST: Classroom/Add
-    //   [HttpPost]
-    //   [Authorize]
-    //   [ValidateAntiForgeryToken]
-    //    public ActionResult Add(DateTime start, DateTime end, string code)
-    //    {
-    //        var classroom = new Classroom
-    //        {
-    //            semesterStart = start,
-    //            semesterEnd = end,
-    //            classCode = code
-    //        };
-    //        ClassroomBinding.SaveRoom(classroom);
-    //    }
-    //}
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(DateTime start, DateTime end, string code)
+        {
+            Teacher t = UserBinding.getTeacher(User.Identity.GetUserId());
+            
+            var classroom = new Classroom
+            {
+                semesterStart = start,
+                semesterEnd = end,
+                classCode = code,
+                teacherID = t.TeacherID
+            };
+            ClassroomBinding.SaveRoom(classroom);
+            return View();
+        }
+    }
 }
