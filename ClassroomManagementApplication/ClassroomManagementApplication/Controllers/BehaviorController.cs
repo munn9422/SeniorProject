285112725a065b34499ed1cb66cef6bca1ee9443
Chris Quinn <cmquinn976@gmail.com>
2018-04-28 15:23:58 -0600

    started behavior work

---
+++ ClassroomManagementApplication/ClassroomManagementApplication/Controllers/BehaviorController.cs
@@ -0,0 +1,40 @@
+﻿using ClassroomManagementApplication.Models;
+using System;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity;
+
+namespace ClassroomManagementApplication.Controllers
+{
+    public class BehaviorController : Controller
+    {
+        // GET: Behavior
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: Behavior/Add
+        [Authorize]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+
+        //POST: Behavior/Add
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(string title, string desc, decimal point)
+        {
+            var behavior = new BehaviorType
+            {
+                behaviorTitle = title,
+                behaviorDescription = desc,
+                pointValue = point
+            };
+        }
+    }
+}
