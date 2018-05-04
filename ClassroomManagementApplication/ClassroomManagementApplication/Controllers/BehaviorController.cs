d12f625274773d1f801b0e0a029dbdfc7d4a176b
munn9422 <munn9422@bears.unco.edu>
2018-05-04 15:24:34 -0600

    AddType Feature

--- ClassroomManagementApplication/ClassroomManagementApplication/Controllers/BehaviorController.cs
+++ ClassroomManagementApplication/ClassroomManagementApplication/Controllers/BehaviorController.cs
@@ -9,6 +9,43 @@ namespace ClassroomManagementApplication.Controllers
 {
     public class BehaviorController : Controller
     {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        public BehaviorController()
+        {
+        }
+
+        public BehaviorController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
         // GET: Behavior
         public ActionResult Index()
         {
@@ -33,23 +70,27 @@ namespace ClassroomManagementApplication.Controllers
 
         //GET: Behavior/AddType
         [Authorize]
-        public ActionResult AddType()
+        public ActionResult AddType(string classcode)
         {
-            return View();
+            AddTypeViewModel model = new AddTypeViewModel();
+            model.ClassCode = classcode;
+            return View(model);
         }
 
         //POST: Behavior/AddType
         [HttpPost]
         [Authorize]
         [ValidateAntiForgeryToken]
-        public ActionResult AddType(string title, string desc, decimal point)
+        public ActionResult AddType(AddTypeViewModel type)
         {
+            //TODO null check
             var behavior = new BehaviorType
             {
                 behaviorID = BehaviorBinding.GenerateBehaviorId(),
-                behaviorTitle = title,
-                behaviorDescription = desc,
-                pointValue = point
+                behaviorTitle = type.Title,
+                behaviorDescription = type.Description,
+                pointValue = type.PointValue,
+                classID = ClassroomBinding.GetClassroomFromCode(type.ClassCode).classID
             };
 
             BehaviorBinding.SaveBehavior(behavior);
